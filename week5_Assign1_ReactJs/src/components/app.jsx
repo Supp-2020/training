bdbe0b102c678e4808f6c69485dd5552f0768718
Supreet Pradhan <73893884+Supp-2020@users.noreply.github.com>
2023-11-06 21:50:33 +0530

    Add files via upload

---
+++ week5_Assign1_ReactJs/src/components/app.jsx
@@ -0,0 +1,17 @@
+import { Component } from "react";
+import Header from "./header";
+import Main from "./main";
+import Footer from "./footer";
+
+class App extends Component{
+  render(){
+    return <div>
+              <Header/>
+              <Main/>
+              <Footer/>
+            </div>
+
+  }
+}
+
+export default App;
